fc6dada61b875b3443a9d9eae45b429e29323dfb
Julia Ybanez <ybanez@cpp.edu>
2023-10-05 13:34:10 -0700

    initial

---
+++ Assignment_2/decision_tree_2.py
@@ -0,0 +1,69 @@
+#-------------------------------------------------------------------------
+# AUTHOR: your name
+# FILENAME: title of the source file
+# SPECIFICATION: description of the program
+# FOR: CS 4210- Assignment #2
+# TIME SPENT: how long it took you to complete the assignment
+#-----------------------------------------------------------*/
+
+#IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
+# dictionaries, lists, and arrays
+
+#importing some Python libraries
+from sklearn import tree
+import csv
+
+dataSets = ['contact_lens_training_1.csv', 'contact_lens_training_2.csv', 'contact_lens_training_3.csv']
+
+for ds in dataSets:
+
+    dbTraining = []
+    X = []
+    Y = []
+
+    #reading the training data in a csv file
+    with open(ds, 'r') as csvfile:
+         reader = csv.reader(csvfile)
+         for i, row in enumerate(reader):
+             if i > 0: #skipping the header
+                dbTraining.append (row)
+
+    #transform the original categorical training features to numbers and add to the 4D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3
+    # so X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
+    #--> add your Python code here
+    # X =
+
+    #transform the original categorical training classes to numbers and add to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
+    #--> add your Python code here
+    # Y =
+
+    #loop your training and test tasks 10 times here
+    for i in range (10):
+
+       #fitting the decision tree to the data setting max_depth=3
+       clf = tree.DecisionTreeClassifier(criterion = 'entropy', max_depth=3)
+       clf = clf.fit(X, Y)
+
+       #read the test data and add this data to dbTest
+       #--> add your Python code here
+       # dbTest =
+
+       for data in dbTest:
+           #transform the features of the test instances to numbers following the same strategy done during training,
+           #and then use the decision tree to make the class prediction. For instance: class_predicted = clf.predict([[3, 1, 2, 1]])[0]
+           #where [0] is used to get an integer as the predicted class label so that you can compare it with the true label
+           #--> add your Python code here
+
+           #compare the prediction with the true label (located at data[4]) of the test instance to start calculating the accuracy.
+           #--> add your Python code here
+
+    #find the average of this model during the 10 runs (training and test set)
+    #--> add your Python code here
+
+    #print the average accuracy of this model during the 10 runs (training and test set).
+    #your output should be something like that: final accuracy when training on contact_lens_training_1.csv: 0.2
+    #--> add your Python code here
+
+
+
+
---
+++ Assignment_2/knn.py
@@ -0,0 +1,59 @@
+#-------------------------------------------------------------------------
+# AUTHOR: your name
+# FILENAME: title of the source file
+# SPECIFICATION: description of the program
+# FOR: CS 4210- Assignment #2
+# TIME SPENT: how long it took you to complete the assignment
+#-----------------------------------------------------------*/
+
+#IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
+
+#importing some Python libraries
+from sklearn.neighbors import KNeighborsClassifier
+import csv
+
+db = []
+
+#reading the data in a csv file
+with open('binary_points.csv', 'r') as csvfile:
+  reader = csv.reader(csvfile)
+  for i, row in enumerate(reader):
+      if i > 0: #skipping the header
+         db.append (row)
+
+#loop your data to allow each instance to be your test set
+for ? in db:
+
+    #add the training features to the 2D array X removing the instance that will be used for testing in this iteration. For instance, X = [[1, 3], [2, 1,], ...]]. Convert each feature value to
+    # float to avoid warning messages
+    #--> add your Python code here
+    # X =
+
+    #transform the original training classes to numbers and add to the vector Y removing the instance that will be used for testing in this iteration. For instance, Y = [1, 2, ,...]. Convert each
+    #  feature value to float to avoid warning messages
+    #--> add your Python code here
+    # Y =
+
+    #store the test sample of this iteration in the vector testSample
+    #--> add your Python code here
+    #testSample =
+
+    #fitting the knn to the data
+    clf = KNeighborsClassifier(n_neighbors=1, p=2)
+    clf = clf.fit(X, Y)
+
+    #use your test sample in this iteration to make the class prediction. For instance:
+    #class_predicted = clf.predict([[1, 2]])[0]
+    #--> add your Python code here
+
+    #compare the prediction with the true label of the test instance to start calculating the error rate.
+    #--> add your Python code here
+
+#print the error rate
+#--> add your Python code here
+
+
+
+
+
+
