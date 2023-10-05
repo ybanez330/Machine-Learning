e18a6a395be3f97cdf81cf5c48686e3d688e8b21
Julia Ybanez <ybanez@cpp.edu>
2023-10-05 14:45:59 -0700

    commit assignment2 number 3e

--- Assignment_2/knn.py
+++ Assignment_2/knn.py
@@ -3,7 +3,7 @@
 # FILENAME: title of the source file
 # SPECIFICATION: description of the program
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 1 hour cummulatively
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
@@ -22,21 +22,30 @@ with open('binary_points.csv', 'r') as csvfile:
          db.append (row)
 
 #loop your data to allow each instance to be your test set
-for ? in db:
+errors = 0
+total = 0
+for i, instance in enumerate(db):
 
     #add the training features to the 2D array X removing the instance that will be used for testing in this iteration. For instance, X = [[1, 3], [2, 1,], ...]]. Convert each feature value to
     # float to avoid warning messages
-    #--> add your Python code here
-    # X =
+    X = []
+    for j, val, in enumerate(db):
+       if i != j:
+          X.append([float(val[0]),float(val[1])])
 
     #transform the original training classes to numbers and add to the vector Y removing the instance that will be used for testing in this iteration. For instance, Y = [1, 2, ,...]. Convert each
     #  feature value to float to avoid warning messages
-    #--> add your Python code here
-    # Y =
+    Y = []
+    for j, val in enumerate(db):
+       if i != j:
+          if val[2] == '-':
+             Y.append(float(0))
+          else:
+            Y.append(float(1)) 
 
     #store the test sample of this iteration in the vector testSample
     #--> add your Python code here
-    #testSample =
+    testSample = [[float(instance[0]), float(instance[1])]]
 
     #fitting the knn to the data
     clf = KNeighborsClassifier(n_neighbors=1, p=2)
@@ -44,13 +53,18 @@ for ? in db:
 
     #use your test sample in this iteration to make the class prediction. For instance:
     #class_predicted = clf.predict([[1, 2]])[0]
-    #--> add your Python code here
+    class_predicted = clf.predict(testSample)[0]
 
     #compare the prediction with the true label of the test instance to start calculating the error rate.
-    #--> add your Python code here
+    comparison = 0
+    if instance[2] == '+':
+        comparison = 1
+    if class_predicted != comparison:
+        errors = errors + 1
+    total = total + 1
 
 #print the error rate
-#--> add your Python code here
+print("error rate is " + str(float(errors/total)))
 
 
 
